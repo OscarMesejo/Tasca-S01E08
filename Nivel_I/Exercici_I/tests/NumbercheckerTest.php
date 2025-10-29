bbcba5ca1041aea6de627559de169a078c057777
Oscar Mesejo <oscar.mesejo@gmail.com>
2025-10-29 13:00:37 +0100

    commit ex1

--- Nivel_I/Exercici_I/tests/NumbercheckerTest.php
+++ Nivel_I/Exercici_I/tests/NumbercheckerTest.php
@@ -4,14 +4,43 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
-Final class NumbercheckerTest extends TestCase{
+Final class NumberCheckerTest extends TestCase{
 
     #[TestDox("Comprobar si un número es par")]
     #[Test]
-    public function IsEven(): void{
+    public function testEvenNumber(): void{
         $numberChecker = new NumberChecker(8);
-        $this -> assertSame($numberChecker -> isEven(8));
+        $this -> assertSame(true, $numberChecker -> isEven());
     }
+    
+    #[TestDox("Comprobar si un número es impar")]
+    #[Test]
+    public function testOddNumber(): void{
+        $numberChecker = new NumberChecker(3);
+        $this -> assertSame(false, $numberChecker -> isEven());
+    }
+
+    #[TestDox("Comprobar si un número es positivo")]
+    #[Test]
+    public function testPositiveNumber(): void{
+        $numberChecker = new NumberChecker(8);
+        $this -> assertSame(true, $numberChecker-> isPositive());
+    }
+
+    #[TestDox("Comprobar si un número es negativo")]
+    #[Test]
+    public function testNegativeNumber(): void{
+        $numberChecker = new NumberChecker(-3);
+        $this -> assertSame(false, $numberChecker-> isPositive());
+    }
+
+    #[TestDox("Comprobar si el número cero no es positivo")]
+    #[Test]
+    public function testZeroNumber(): void {
+        $numberChecker = new NumberChecker(0);
+        $this ->assertSame(false, $numberChecker->isPositive());
+    }
+
 }
 
 ?>
